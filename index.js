92590fd7fd66b7a0d5157b6ef44c80a475e02dca
Jithin Emmanuel <jithine@verizonmedia.com>
2022-01-18 12:37:36 -0800

    fix: make if check POSIX complaint (#201)

--- index.js
+++ index.js
@@ -599,7 +599,7 @@ class GithubScm extends Scm {
 
         command.push(
             // eslint-disable-next-line no-template-curly-in-string
-            "export SD_GIT_WRAPPER=\"$(if [[ `uname` = 'Darwin' || ${SD_HAB_ENABLED:-false} = 'false' ]]; " +
+            "export SD_GIT_WRAPPER=\"$(if [ `uname` = 'Darwin' ] || [ ${SD_HAB_ENABLED:-false} = 'false' ]; " +
                 "then echo 'eval'; " +
                 "else echo 'sd-step exec core/git'; fi)\""
         );
