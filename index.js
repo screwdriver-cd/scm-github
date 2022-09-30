5ce592586a7df13b4dba1828dbf4da2a2df8c8b9
Ibuki <ibu1224@users.noreply.github.com>
2022-10-01 03:49:40 +0900

    fix(2734): Builds cannot be started if a pipeline has more than 5 invalid admins (#210)

--- index.js
+++ index.js
@@ -243,7 +243,16 @@ class GithubScm extends Scm {
             // Do not retry when there is a 4XX error
             shouldRetry: err => err && err.statusCode && !(err.statusCode >= 400 && err.statusCode < 500),
             retry: this.config.fusebox.retry,
-            breaker: this.config.fusebox.breaker
+            breaker: {
+                ...this.config.fusebox.breaker,
+                errorFn(err) {
+                    if (err.statusCode) {
+                        return !(err.statusCode >= 400 && err.statusCode < 500);
+                    }
+
+                    return true;
+                }
+            }
         });
     }
 
