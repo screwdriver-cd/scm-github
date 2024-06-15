c7fe0f0be88937cd8f370582b97542db154a2d05
y-oksaku <43719835+y-oksaku@users.noreply.github.com>
2024-06-16 08:48:59 +0900

    feat: fetch commit before git reset (#228)

--- index.js
+++ index.js
@@ -848,6 +848,7 @@ class GithubScm extends Scm {
                 command.push(`$SD_GIT_WRAPPER "git reset --hard '${doubleQuoteEscapedBranch}' --"`);
                 command.push(`echo 'Reset to ${singleQuoteEscapedBranch}'`);
             } else {
+                command.push(`$SD_GIT_WRAPPER "git fetch --depth 1 origin '${config.sha}'"`);
                 command.push(`$SD_GIT_WRAPPER "git reset --hard '${config.sha}' --"`);
                 command.push(`echo 'Reset to ${config.sha}'`);
             }
