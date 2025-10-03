952dd70a4451f3b98075e59da0fbed0318cd8679
yakanechi <138551445+yakanechi@users.noreply.github.com>
2025-10-03 09:52:07 +0900

    fix: shallow clone submodules default

--- index.js
+++ index.js
@@ -686,7 +686,7 @@ class GithubScm extends Scm {
                 'if [ ! -z $GIT_RECURSIVE_CLONE ] && [ $GIT_RECURSIVE_CLONE = false ]; then',
                 '    export GIT_RECURSIVE_OPTION="";',
                 'else',
-                '    export GIT_RECURSIVE_OPTION="--recursive";',
+                '    export GIT_RECURSIVE_OPTION="--recursive --shallow-submodules";',
                 'fi'
             ]),
             // Set sparse option
@@ -948,7 +948,7 @@ class GithubScm extends Scm {
                     '    if [ ! -z $GIT_RECURSIVE_CLONE ] && [ $GIT_RECURSIVE_CLONE = false ]; then',
                     '        $SD_GIT_WRAPPER "git submodule init";',
                     '    else',
-                    '        $SD_GIT_WRAPPER "git submodule update --init --recursive";',
+                    '        $SD_GIT_WRAPPER "git submodule update --init --recursive --depth 1";',
                     '    fi;',
                     'fi'
                 ])
