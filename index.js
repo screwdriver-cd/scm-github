a3192b7ea80f2a7cdd2b91d24a34351bbe4ce58e
Dayanand Sagar <sagar1312@gmail.com>
2026-05-29 09:31:51 -0700

    fix: prefer x-hub-signature-256 for webhook signature verification (#259)

--- index.js
+++ index.js
@@ -1793,7 +1793,7 @@ class GithubScm extends Scm {
      *                                   payload
      */
     async _parseHook(payloadHeaders, webhookPayload) {
-        const signature = payloadHeaders['x-hub-signature'];
+        const signature = payloadHeaders['x-hub-signature-256'] || payloadHeaders['x-hub-signature'];
         const type = payloadHeaders['x-github-event'];
         const hookId = payloadHeaders['x-github-delivery'];
         const scmContexts = this._getScmContexts();
@@ -1802,9 +1802,13 @@ class GithubScm extends Scm {
 
         const checkoutSshHost = this.config.gheHost ? this.config.gheHost : 'github.com';
 
+        if (!signature) {
+            throwError('Missing webhook signature', 400);
+        }
+
         // eslint-disable-next-line no-underscore-dangle
         if (!(await verify(this.config.secret, webhookPayload, signature))) {
-            throwError('Invalid x-hub-signature', 400);
+            throwError('Invalid webhook signature', 400);
         }
 
         const parsedWebhookPayload = JSON.parse(webhookPayload);
